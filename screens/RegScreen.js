adc07d5fef8b40e5043b3a2e5b2864420ce3a647
EtzMartinez <122953490+EtzMartinez@users.noreply.github.com>
2023-04-16 02:23:50 -0400

    Add files via upload

--- screens/RegScreen.js
+++ screens/RegScreen.js
@@ -1,7 +1,7 @@
 // Loginscreen.js
 
 import React, { Component, useState } from 'react';
-import { ActivityIndicator, Button, View, Text, TextInput } from 'react-native';
+import { ImageBackground, Button, View, Text, TextInput } from 'react-native';
 
 global.localName = '';
 global.password = '';
@@ -23,72 +23,75 @@ export default class RegisterScreen extends Component {
 
   render(){
     return(
-        <View style={{ backgroundColor:'#0cc00c', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
-        <Text style={{fontSize:30}}>Register Screen </Text>
+      <ImageBackground source ={require('../assets/RegisterBG.jpg')} style ={{flex:1}}>
+      <View style={styles.container}>
+      <View style={styles.body}>
+        <View style={styles.titleContainer}>
+          <Text style={{fontSize:30}}>{" "}Register </Text>
+        </View>
         <Text style={{fontSize:20}}> </Text> 
         {/* Above line puts a gap between words */}
 
         <View style={{alignItems: 'flex-end'}}>
-            {/* First Name */}
-            <View style={{ flexDirection:'row' }}>
-                <Text style={{fontSize:20}}>First Name: </Text>
-                <TextInput
-                style={{height: 30,fontSize:20, backgroundColor:'#ffffff'}}
-                placeholder="First Name"
-                onChangeText={(val) => { this.changeFirstNameHandler(val) }}
-                />        
-            </View>
-            <Text style={{fontSize:20}}> </Text>
-            {/* Last Name */}
-            <View style={{ flexDirection:'row' }}>
-                <Text style={{fontSize:20}}>Last Name: </Text>
-                <TextInput
-                style={{height: 30,fontSize:20, backgroundColor:'#ffffff'}}
-                placeholder="Last Name"
-                onChangeText={(val) => { this.changeLastNameHandler(val) }}
-                />
-            </View>
-            <Text style={{fontSize:20}}> </Text>
-            {/* Email */}
-            <View style={{ flexDirection:'row' }}>
-                <Text style={{fontSize:20}}>E-mail: </Text>
-                <TextInput
-                style={{height: 30,fontSize:20, backgroundColor:'#ffffff'}}
-                placeholder="Email"
-                onChangeText={(val) => { this.changeEmailHandler(val) }}
-                />
-            </View>
-            <Text style={{fontSize:20}}> </Text>
-            {/* Login */}
-            <View style={{ flexDirection:'row' }}>
-                <Text style={{fontSize:20}}>Login: </Text>
-                <TextInput
-                style={{height: 30,fontSize:20, backgroundColor:'#ffffff'}}
-                placeholder="Login"
-                onChangeText={(val) => { this.changeLoginNameHandler(val) }}
-                />
-            </View>
-            <Text style={{fontSize:20}}> </Text>
-            {/* Password */}
-            <View style={{ flexDirection:'row' }}>
-                <Text style={{fontSize:20}}>Password: </Text>
-                <TextInput
-                style={{height: 30,fontSize:20, backgroundColor:'#ffffff'}}
-                placeholder="Last Name"
-                onChangeText={(val) => { this.changePasswordHandler(val) }}
-                />
-            </View>
-            <Text style={{fontSize:20}}>{this.state.message} </Text>
-
-
+          {/* First Name */}
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{fontSize:20}}>First Name: </Text>
+            <TextInput
+            style={styles.textInp}
+            placeholder=" First Name"
+            onChangeText={(val) => { this.changeFirstNameHandler(val) }}
+            />        
+          </View>
+          <Text style={{fontSize:20}}> </Text>
+          {/* Last Name */}
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{fontSize:20}}>Last Name: </Text>
+            <TextInput
+            style={styles.textInp}
+            placeholder=" Last Name"
+            onChangeText={(val) => { this.changeLastNameHandler(val) }}
+            />
+          </View>
+          <Text style={{fontSize:20}}> </Text>
+          {/* Email */}
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{fontSize:20}}>E-mail: </Text>
+            <TextInput
+            style={styles.textInp}
+            placeholder=" Email"
+            onChangeText={(val) => { this.changeEmailHandler(val) }}
+            />
+          </View>
+          <Text style={{fontSize:20}}> </Text>
+          {/* Login */}
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{fontSize:20}}>Login: </Text>
+            <TextInput
+            style={styles.textInp}
+            placeholder=" Login"
+            onChangeText={(val) => { this.changeLoginNameHandler(val) }}
+            />
+          </View>
+          <Text style={{fontSize:20}}> </Text>
+          {/* Password */}
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{fontSize:20}}>Password: </Text>
+            <TextInput
+            style={styles.textInp}
+            placeholder=" Last Name"
+            onChangeText={(val) => { this.changePasswordHandler(val) }}
+            />
+          </View>
+          <Text style={{fontSize:15}}>{this.state.message} </Text>
         </View>
 
         <Button
-            title="Register"
-            onPress={this.handleClick}
+          title="Register"
+          onPress={this.handleClick}
         />
-        </View>
+      </View>
+      </View>
+      </ImageBackground>
     );
   }
 
@@ -119,7 +122,7 @@ export default class RegisterScreen extends Component {
             global.firstName = res.firstName;
             global.lastName = res.lastName;
             global.userId = res.id;
-            this.setState({message: "New User Created... hopefully" });
+            this.setState({message: "New User Created. Please register your E-mail!" });
         }
     }
     catch(e)
@@ -149,4 +152,46 @@ export default class RegisterScreen extends Component {
     global.password = val;
   }  
 
+}
+
+
+const styles = {
+  container: {
+    flex: 1,                  // takes up the available width of the container ( relative to other flex values of other objects)
+    justifyContent: 'center', // aligns items by Row
+    alignItems: 'center',     // aligns items by Column
+    //backgroundColor: '#aaaaa0',
+  },
+  body: {
+    borderRadius: 10,
+    backgroundColor: '#F8CD46',
+    paddingVertical: 15,              // how many pixels around the text that this View will also take up
+    paddingHorizontal: 25,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 10,    //establishes layering
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  textInp: {
+    height: 30,
+    width: 120,
+    fontSize:20, 
+    backgroundColor:'#ffffff',
+    borderRadius: 5
+  },
+  titleContainer: {
+    alignSelf: 'center',
+    padding: 5,
+    borderBottomWidth: 3,
+    borderBottomColor: 'black',
+  },
 }
